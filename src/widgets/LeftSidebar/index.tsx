cf0e847f1d85c76430735b541ed0496a1ad39a81
zero0205 <etety39@naver.com>
2025-05-09 16:53:51 +0900

    :memo: [Docs]: Biome 규칙 수정
    - biome.json을 제외하고는 biome이 자동으로 린팅&포매팅하며 변경된 파일들

--- src/widgets/LeftSidebar/index.tsx
+++ src/widgets/LeftSidebar/index.tsx
@@ -3,18 +3,18 @@ import CalendarList from "./CalendarList";
 import UserInfo from "./UserInfo";
 
 export function LeftSidebar() {
-	return (
-		<div className="flex h-screen w-64 flex-col border-grayscale-300 border-r">
-			<div className="p-4">
-				<h1 className="font-semibold text-bold-l text-primary-main">Schedo</h1>
-			</div>
+  return (
+    <div className="flex h-screen w-64 flex-col border-grayscale-300 border-r">
+      <div className="p-4">
+        <h1 className="font-semibold text-bold-l text-primary-main">Schedo</h1>
+      </div>
 
-			<div className="h-10 flex-1 px-2">
-				<CalendarHeader />
-				<CalendarList />
-			</div>
+      <div className="h-10 flex-1 px-2">
+        <CalendarHeader />
+        <CalendarList />
+      </div>
 
-			<UserInfo />
-		</div>
-	);
+      <UserInfo />
+    </div>
+  );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -5,16 +5,16 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), tailwindcss()],
-	resolve: {
-		alias: {
-			"@": path.resolve(__dirname, "./src"),
-			"@app": path.resolve(__dirname, "./src/app"),
-			"@pages": path.resolve(__dirname, "./src/pages"),
-			"@widgets": path.resolve(__dirname, "./src/widgets"),
-			"@features": path.resolve(__dirname, "./src/features"),
-			"@entities": path.resolve(__dirname, "./src/entities"),
-			"@shared": path.resolve(__dirname, "./src/shared"),
-		},
-	},
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@app": path.resolve(__dirname, "./src/app"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@widgets": path.resolve(__dirname, "./src/widgets"),
+      "@features": path.resolve(__dirname, "./src/features"),
+      "@entities": path.resolve(__dirname, "./src/entities"),
+      "@shared": path.resolve(__dirname, "./src/shared"),
+    },
+  },
 });
