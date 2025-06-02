fd8f8aff338a9bf86f5aa4640566b21f1e41ed3e
zero0205 <etety39@naver.com>
2025-06-02 17:33:28 +0900

    :sparkles: [Feat]: 하단 버튼 그룹을 별도의 컴포넌트로 분리
    - 기능과 버튼 텍스트 정도만 다를뿐 UI는 동일하므로 별도의 컴포넌트로 분리

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -3,3 +3,6 @@ export { Button } from "./button";
 export { ScrollArea, ScrollBar } from "./scroll-area";
 export { Calendar } from "./calendar";
 export * from "./popover";
+export * from "./select";
+export * from "./switch";
+export { ButtonGroup } from "./ButtonGroup";
