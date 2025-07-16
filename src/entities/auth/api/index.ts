e7a0166aad7329da875db432fc424106f9a2ff83
zero0205 <etety39@naver.com>
2025-07-17 00:06:49 +0900

    :recycle: [Refactor]: URL 객체를 사용하여 로그인 url 관리

--- src/entities/auth/api/index.ts
+++ src/entities/auth/api/index.ts
@@ -3,8 +3,10 @@ import { devLogger } from "@/shared/lib";
 import { useAuthStore } from "@/shared/stores";
 
 export const requestLogIn = (provider: "google" | "kakao") => {
-  const redirectUri = `${window.location.origin}`;
-  window.location.href = `${import.meta.env.VITE_API_BASE_URL}/oauth2/authorization/${provider}?redirect_uri=${encodeURIComponent(redirectUri)}`;
+  const redirectUri = window.location.origin;
+  const url = new URL(`${import.meta.env.VITE_API_BASE_URL}/oauth2/authorization/${provider}`);
+  url.searchParams.set("redirect_uri", redirectUri);
+  window.location.href = url.href;
 };
 
 export const requestLogOut = () => {
