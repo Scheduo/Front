da8c2ff642c4127cf2eded95e773c48bf90c3f76
zero0205 <etety39@naver.com>
2025-07-25 00:31:30 +0900

    :sparkles: [Feat]: NotificationApi 타입 수정
    - getNotifications 응답 타입 추가(`GetNotificationsResponse`)
    - deleteNotification 메서드의 파라미터 타입을 `number`로 변경

--- src/entities/notification/api/notificationApi.ts
+++ src/entities/notification/api/notificationApi.ts
@@ -1,11 +1,12 @@
 import { axiosInstance } from "@/shared/api";
+import type { GetNotificationsResponse } from "./types";
 
 export const notificationApi = {
-  getNotifications: async () => {
+  getNotifications: async (): Promise<GetNotificationsResponse> => {
     const response = await axiosInstance.get("/notifications");
     return response.data.data;
   },
 
-  deleteNotification: async (notificationId: string) =>
+  deleteNotification: async (notificationId: number) =>
     await axiosInstance.delete(`/notifications/${notificationId}/read`),
 };
