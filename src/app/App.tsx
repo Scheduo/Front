c07d67c0dc1fccb7a0f8da90b16dce8983e85303
zero0205 <etety39@naver.com>
2025-08-08 20:43:28 +0900

    :sparkles: [Feat]: QueryClient 설정

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,16 @@
+import { QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter } from "react-router";
+import { queryClient } from "@/shared/tanstack-query";
 import { Toaster } from "@/shared/ui";
 import { Router } from "./routes";
 
 function App() {
   return (
     <BrowserRouter>
-      <Router />
-      <Toaster />
+      <QueryClientProvider client={queryClient}>
+        <Router />
+        <Toaster />
+      </QueryClientProvider>
     </BrowserRouter>
   );
 }
