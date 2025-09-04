498775dd109b247df1e6e8cacb43fd6cd4ce56d6
zero0205 <etety39@naver.com>
2025-09-05 00:01:12 +0900

    :bug: [Fix]: UpdateSchedule 리턴 값 수정
    - 리턴 타입이 void이므로 리턴하는 값이 없도록 변경

--- src/entities/schedule/api/api.ts
+++ src/entities/schedule/api/api.ts
@@ -46,8 +46,7 @@ export const scheduleApi = {
     request: UpdateScheduleRequest,
   ): Promise<void> => {
     const url = `/calendars/${calendarId}/schedules/${scheduleId}`;
-    const response = await axiosInstance.patch(url, request, { params: { date } });
-    return response.data.data;
+    axiosInstance.patch(url, request, { params: { date } });
   },
 
   // 기존 함수들 (향후 구현 예정)
